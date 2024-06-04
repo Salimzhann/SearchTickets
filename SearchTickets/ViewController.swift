d266bf7efb029c2224bcd67d11959c1f485f7e22
Salimzhann <manassalimzhan@icloud.com>
2024-06-04 10:36:20 +0500

    Initial Commit

---
+++ SearchTickets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchTickets
+//
+//  Created by Manas Salimzhan on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
